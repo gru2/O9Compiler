d90caba769cbdd2608d404f1ac8434ff44e33c66
gru <Radovan.Obradovic@gmail.com>
2014-08-29 11:49:14 +0200

    Unit test for class Instruction added.

---
+++ src/InstructionTest.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+
+""" Unittest class Instruction. """
+
+import Instruction
+import unittest
+
+class InstructionTest(unittest.TestCase):
+
+    def test01(self):
+        x = Instruction.Instruction()
+
+
+if __name__ == '__main__':
+    unittest.main()
+
