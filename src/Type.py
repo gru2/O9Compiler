cc7e1d7466afb3382082d2dc30a5b945cc94029e
gru <Radovan.Obradovic@gmail.com>
2014-09-13 06:29:23 +0200

    Type added.

---
+++ src/Type.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2
+
+""" Implementation of class Type. """
+
+class Type:
+
+    def __init__(self):
+        self.width = 32
+        self.isSigned = True
+        self.isFloat = False
